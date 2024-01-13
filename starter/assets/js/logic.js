a621fdd5a585f984d8eb56fbe3cde69168b609b1
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-01-13 23:38:38 +0000

    managed to get the question showing, choices showing, right answer showing on console, and next button showing up too.

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -13,6 +13,11 @@ function startQuiz() {
   // Show questions
   document.getElementById('questions').classList.remove('hide');
 
+    // // Start timer
+    // var time = questions.length * 15;
+    // //var timeInterval;
+    // var timer = document.getElementById('timer');
+    
     // // Start countdown
     // timeInterval = setInterval(function() {
     //     time--;
@@ -27,24 +32,24 @@ function startQuiz() {
     //     }
     // }, 1000);
  
-}
+// /}
 
-function endQuiz() {
-    // Hide questions
-    document.getElementById('questions').classList.add('hide');
+// function endQuiz() {
+//     // Hide questions
+//     document.getElementById('questions').classList.add('hide');
 
-    // Show end screen
-    document.getElementById('end-screen').classList.remove('hide');
+//     // Show end screen
+//     document.getElementById('end-screen').classList.remove('hide');
 
-    // Update final score
-    // updateFinalScore();
-}
+//     // Update final score
+//     // updateFinalScore();
+// }
 
 function presentQuestion() {    
     // Get current question
     var currentQuestion = questions[currentQuestionIndex];
 
-//    for (questions = 0; i < questions.length; i++) {
+     //for (questions = 0; i < questions.length; i++) {
     // Display question
     document.getElementById('question-title').textContent = currentQuestion.question;
     // Display options
@@ -52,17 +57,19 @@ function presentQuestion() {
     document.getElementById('choices-2').textContent = currentQuestion.options[1];
     document.getElementById('choices-3').textContent = currentQuestion.options[2];
     document.getElementById('choices-4').textContent = currentQuestion.options[3];
+}
 
-    // // Add event listener to check answer
-     document.getElementById('answer').addEventListener('click', checkAnswer);
+    presentQuestion();
+// //     // // Add event listener to check answer
+      document.getElementById('questions').addEventListener('click', checkAnswer);
 
     // Check answer
     function checkAnswer(answer) {
         // Get the text of the clicked choice
-        var answer = answer.target.textContent;
+        var correctAnswer = answer.target.textContent;
       
         // Check if the choice is correct
-        if (answer === questions[currentQuestionIndex].answer) {
+        if (correctAnswer === questions[currentQuestionIndex].answer) {
           // The choice is correct
           console.log("Correct!");
         } else {
@@ -70,32 +77,27 @@ function presentQuestion() {
           console.log("Incorrect!");
         }
       }
-      currentQuestionIndex++;
-
-  }
-
-
-
-
-    // function nextQuestion() {   
-
-    //     // Check if there are more questions
-    //     if (currentQuestionIndex < questions.length - 1) {
-    //         // There are more questions
-    //         currentQuestionIndex++;
-    //         presentQuestion();
-    //     // } else {
-    //     //     // There are no more questions
-    //     //     endQuiz();
-    //      }
-
-
-    // // Get the next button
-    // document.getElementById('next').addEventListener('click', nextQuestion);
-
-
     
-    presentQuestion();
-    checkAnswer();
+      checkAnswer();
+  
+ // Get the next button
+    document.getElementById('next').addEventListener('click', nextQuestion);
+
+    function nextQuestion() {   
+
+        // Check if there are more questions
+        if (currentQuestionIndex < questions.length - 1) {
+            // There are more questions
+            currentQuestionIndex++;
+            presentQuestion();
+        } else {
+            // There are no more questions
+            endQuiz();
+         }
+
+        }
+   
+    
     nextQuestion();
-    
+    endQuiz();
+    }
