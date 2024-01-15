d7957027d68000a875b172b8c731dc48213ad6b8
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-01-15 23:07:56 +0000

    removed highscore

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -5,10 +5,10 @@ let score = 0;
 function updateScore(answer) {
     if (answer === 'correct') {
         score++;
-    } else {
-        penalizeScore();
     }
+    updateFinalScore(); // Call the updateFinalScore function after updating the score
 }
+
 //incorrect answer will penalize the score by ten seconds
 function penalizeScore() {
    score -= 10;
@@ -27,12 +27,25 @@ function clearScore() {
 
 
 
-// Example usage
-updateScore('correct');
-console.log(score); // Output: 1
 
 // update the final score
 function updateFinalScore() {
     document.getElementById('final-score').innerHTML = score;
-}   
+}
+
+// Example usage
+updateScore('correct');
+console.log(score); // Output: 1
+
+//save initials and score to localStorage use submit button
+function saveScore() {
+    // Get the initials from the input field
+    var initials = document.getElementById('initials').value;
+    // Save the initials and score to localStorage
+    localStorage.setItem('initials', initials);
+    localStorage.setItem('score', score);
+    
+    // Redirect from local storage to highscores page
+    window.location.href = 'highscores.html';
+}
 
